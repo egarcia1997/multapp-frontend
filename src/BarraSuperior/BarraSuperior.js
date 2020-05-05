359423e87d133f0b02a0bd7f26275e504eed09b5
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-05 17:45:38 -0300

    ahora la barra superior tiene sus elementos alineados hacia la derecha, y queda fija arriba

--- src/BarraSuperior/BarraSuperior.js
+++ src/BarraSuperior/BarraSuperior.js
@@ -4,13 +4,15 @@ import "./BarraSuperior.css";
 const barraSuperior = (props) => {
     return (
         <div className="BarraSuperior">
+            {/* los elementos se van a mostrar al reves de como estan aca */}
+            {/* porque en el css tiene el estilo flex-direction: row-reverse */}
+            <img src={props.imagen} alt="Su foto de perfil" />
             <div>
                 <table>
                     <tr><td><b>{props.nombre}</b></td></tr>
                     <tr><td>Cerrar sesión</td></tr>
                 </table>
             </div>
-            <img src={props.imagen} alt="Su foto de perfil" />
         </div>
     );
 }
