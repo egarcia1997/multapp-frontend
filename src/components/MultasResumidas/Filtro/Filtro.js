5ca9340274ada5e7bf165fec8b596fe0dbbd6182
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-19 20:44:33 -0300

    agregado un prop que controla la visibilidad

--- src/components/MultasResumidas/Filtro/Filtro.js
+++ src/components/MultasResumidas/Filtro/Filtro.js
@@ -2,23 +2,25 @@ import React from "react";
 import estilos from "./Filtro.module.css";
 
 const Filtro = (props) => {
-    return (
-        <div className={estilos.Filtro}>
-            <div id="estado">
-                <label htmlFor="noResuelta">No Resuelta</label>
-                <input type="checkbox" id="noResuelta" value="No Resuelta" />
-                <label htmlFor="aceptada">Aceptada</label>
-                <input type="checkbox" id="aceptada" value="Aceptada" />
-                <label htmlFor="rechazada">Rechazada</label>
-                <input type="checkbox" id="rechazada" value="Rechazada" />
+    if (props.visible) {
+        return (
+            <div className={estilos.Filtro}>
+                <div id="estado">
+                    <label htmlFor="noResuelta">No Resuelta</label>
+                    <input type="checkbox" id="noResuelta" value="No Resuelta" />
+                    <label htmlFor="aceptada">Aceptada</label>
+                    <input type="checkbox" id="aceptada" value="Aceptada" />
+                    <label htmlFor="rechazada">Rechazada</label>
+                    <input type="checkbox" id="rechazada" value="Rechazada" />
+                </div>
+                <div id="fecha">
+                    <span>Desde</span><input type="date" />
+                    <span>Hasta</span><input type="date" />
+                </div>
+                <span>DNI</span><input type="number" />
             </div>
-            <div id="fecha">
-                <span>Desde</span><input type="date" />
-                <span>Hasta</span><input type="date" />
-            </div>
-            <span>DNI</span><input type="number" />
-        </div>
-    );
+        );
+    }
 }
 
 export default Filtro;
