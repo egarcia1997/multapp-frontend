66e5822987a031f85b31bdc0f54445ee8736d2ec
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-19 19:51:07 -0300

    creado el componente MultasResumidas y metido ahi la logica de las multas

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
-import MultaResumida from "./components/MultaResumida/MultaResumida";
+import MultasResumidas from "./components/MultasResumidas/MultasResumidas";
 import BarraSuperior from "./components/BarraSuperior/BarraSuperior";
 
 class App extends Component{
   state = {
-    multas: [ // esto contiene los resumenes de todas las multas sacado directamente de la db
-      // lo que esta aca cargado es a modo de ejemplo, se saca al tener acceso a la db
-      {
-        id: "asdasdasd",
-        nombreConductor: "Freeman Gordon",
-        dniConductor: "12.345.678",
-        fecha: "1/01/20",
-        extracto: "Circular por encima de la velocidad máxima permitida",
-        estado: "No resuelta",
-      },
-      {
-        id: "asdasdasd",
-        nombreConductor: "Jensen Adam",
-        dniConductor: "23.456.789",
-        fecha: "2/01/20",
-        extracto: "Circular con RTO vencida al día de la fecha",
-        estado: "Aceptada",
-      },
-      {
-        id: "asdasdasd",
-        nombreConductor: "Yu Morgan",
-        dniConductor: "34.567.890",
-        fecha: "3/01/20",
-        extracto: "Estacionar sobre calzada amarilla",
-        estado: "Rechazada",
-      },
-    ],
     usuario:
     {
       nombre: "Juan Pérez",
@@ -40,43 +13,11 @@ class App extends Component{
   }
 
   render() {
-    let multasParaMostrar = []; // crea un array con las multas que se van a mostrar
-    let numeroDeMultasSinResolver = 0; // el numero total de multas que estan sin resolver
-    let textoDeMultasSinResolver = ""; // el texto que se va a mostrar debajo del h2 que da la bienvenida
-
-    this.state.multas.forEach((multa) => { // ejecuta por cada multa del estado
-      // ACA VAN LAS CONDICIONES DE FILTRADO
-      // if (cumpleConLasCondicionesDeFiltrado)
-      if (multa.estado === "No resuelta") {
-        numeroDeMultasSinResolver++;
-      }
-      multasParaMostrar.push( // crea el componente por cada multa que va a mostrar
-        <MultaResumida id={multa.id}
-        nombreConductor={multa.nombreConductor}
-        dniConductor={multa.dniConductor}
-        fecha={multa.fecha}
-        extracto={multa.extracto}
-        estado={multa.estado}
-        click={() => alert("coso")} />
-      );
-    });
-
-    if (numeroDeMultasSinResolver === 0) {
-      textoDeMultasSinResolver = "No quedan multas sin resolver";
-    } else if (numeroDeMultasSinResolver === 1) {
-      textoDeMultasSinResolver = "Tiene 1 multa sin resolver";
-    } else {
-      textoDeMultasSinResolver = "Tiene " + numeroDeMultasSinResolver + " multas sin resolver";
-    }
-
     return (
-      <div className="App">
+      <Fragment>
         <BarraSuperior nombre={this.state.usuario.nombre} imagen={this.state.usuario.imagen} />
-        <h1>Bienvenido, {this.state.usuario.nombre}</h1>
-        <h2>{textoDeMultasSinResolver}</h2>
-        <button onClick={() => alert("asd")}>Filtrar</button>
-        {multasParaMostrar}
-      </div>
+        <MultasResumidas nombreUsuario={this.state.usuario.nombre} />
+      </Fragment>
     );
   }
 }
---
+++ src/components/MultasResumidas/MultasResumidas.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+//import './MultasResumidas.module.css';
+import MultaResumida from "./MultaResumida/MultaResumida";
+
+class MultasResumidas extends Component{
+    state = {
+      multas: [ // esto contiene los resumenes de todas las multas sacado directamente de la db
+        // lo que esta aca cargado es a modo de ejemplo, se saca al tener acceso a la db
+        {
+          id: "asdasdasd",
+          nombreConductor: "Freeman Gordon",
+          dniConductor: "12.345.678",
+          fecha: "1/01/20",
+          extracto: "Circular por encima de la velocidad máxima permitida",
+          estado: "No resuelta",
+        },
+        {
+          id: "asdasdasd",
+          nombreConductor: "Jensen Adam",
+          dniConductor: "23.456.789",
+          fecha: "2/01/20",
+          extracto: "Circular con RTO vencida al día de la fecha",
+          estado: "Aceptada",
+        },
+        {
+          id: "asdasdasd",
+          nombreConductor: "Yu Morgan",
+          dniConductor: "34.567.890",
+          fecha: "3/01/20",
+          extracto: "Estacionar sobre calzada amarilla",
+          estado: "Rechazada",
+        },
+      ],
+    }
+  
+    render() {
+      let multasParaMostrar = []; // crea un array con las multas que se van a mostrar
+      let numeroDeMultasSinResolver = 0; // el numero total de multas que estan sin resolver
+      let textoDeMultasSinResolver = ""; // el texto que se va a mostrar debajo del h2 que da la bienvenida
+  
+      this.state.multas.forEach((multa) => { // ejecuta por cada multa del estado
+        // ACA VAN LAS CONDICIONES DE FILTRADO
+        // if (cumpleConLasCondicionesDeFiltrado)
+        if (multa.estado === "No resuelta") {
+          numeroDeMultasSinResolver++;
+        }
+        multasParaMostrar.push( // crea el componente por cada multa que va a mostrar
+          <MultaResumida id={multa.id}
+          nombreConductor={multa.nombreConductor}
+          dniConductor={multa.dniConductor}
+          fecha={multa.fecha}
+          extracto={multa.extracto}
+          estado={multa.estado}
+          click={() => alert("coso")} />
+        );
+      });
+  
+      if (numeroDeMultasSinResolver === 0) {
+        textoDeMultasSinResolver = "No quedan multas sin resolver";
+      } else if (numeroDeMultasSinResolver === 1) {
+        textoDeMultasSinResolver = "Tiene 1 multa sin resolver";
+      } else {
+        textoDeMultasSinResolver = "Tiene " + numeroDeMultasSinResolver + " multas sin resolver";
+      }
+  
+      return (
+        <div className="App">
+          <h1>Bienvenido, {this.props.nombreUsuario}</h1>
+          <h2>{textoDeMultasSinResolver}</h2>
+          <button onClick={() => alert("asd")}>Filtrar</button>
+          {multasParaMostrar}
+        </div>
+      );
+    }
+  }
+  
+  export default MultasResumidas;
+  
