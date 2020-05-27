6ad01c8ec2377b5487465010d8e567ee8a0b5faa
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-27 12:06:42 -0300

    definida la baseURL de axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL = "https://multa-app.herokuapp.com"; // esto es para usar la misma url en todas las peticiones http sin tener que andar escribiendo
 
 ReactDOM.render(
   <React.StrictMode>
