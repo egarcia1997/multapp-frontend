3f30db3e2d0172bf550a78119caaa968b2f5fe3b
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-27 13:29:43 -0300

    creado el mensaje de error + cambios menores

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -3,6 +3,15 @@ import estilos from "./Login.module.css";
 import Logo from "../../components/Logo/Logo";
 
 class Login extends Component {
+    // metodo para hacer el login
+    loginHandler() {
+        // mandar al server usuario y contraseña
+        // si es incorrecto hacxer visible el div de clase estilos.Error
+        // si es correcto hacer un if
+        //     si el usuario es un supervisor cargar el componente LayoutSupervisor
+        //     si el usuario es un administrador cargar el componente LayoutAdministrador
+    }
+
     render() {
         return (
             <Fragment>
@@ -10,11 +19,14 @@ class Login extends Component {
                     <div className={estilos.Controles}>
                         <Logo width={185} height={186} />
                         <h1>MultApp</h1>
+                        <div className={estilos.Error}>
+                            El correo electrónico o la contraseña son incorrectos
+                        </div>
                         <label for="email">Correo Electrónico</label>
                         <input type="text" id="email" />
                         <label for="contrasena">Contraseña</label>
                         <input type="password" id="contrasena" />
-                        <button>Iniciar sesión</button>
+                        <button onClick={this.loginHandler}>Iniciar sesión</button>
                         <a href="">¿Olvidó su contraseña?</a>
                     </div>
                 </div>
