4a16220514b96705d48fa09205702f0577bc6d6c
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-29 15:55:04 -0300

    arreglados los estilos

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -15,20 +15,19 @@ class Login extends Component {
     render() {
         return (
             <Fragment>
-                <div className={estilos.Background}>
-                    <div className={estilos.Controles}>
-                        <Logo width={185} height={186} />
-                        <h1>MultApp</h1>
-                        <div className={estilos.Error}>
-                            El correo electrónico o la contraseña son incorrectos
-                        </div>
-                        <label for="email">Correo Electrónico</label>
-                        <input type="text" id="email" />
-                        <label for="contrasena">Contraseña</label>
-                        <input type="password" id="contrasena" />
-                        <button onClick={this.loginHandler}>Iniciar sesión</button>
-                        <a href="">¿Olvidó su contraseña?</a>
+                <div className={estilos.Background}></div>
+                <div className={estilos.Controles}>
+                    <Logo width={185} height={186} />
+                    <h1>MultApp</h1>
+                    <div className={estilos.Error}>
+                        El correo electrónico o la contraseña son incorrectos
                     </div>
+                    <label for="email">Correo Electrónico</label>
+                    <input type="text" id="email" />
+                    <label for="contrasena">Contraseña</label>
+                    <input type="password" id="contrasena" />
+                    <button onClick={this.loginHandler}>Iniciar sesión</button>
+                    <a href="">¿Olvidó su contraseña?</a>
                 </div>
             </Fragment>
         );
