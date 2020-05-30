ecba58238defe034afe7e9f6c04b1037c969ccde
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-05-29 23:13:55 -0300

    creadas varias funciones (que no ahcen nada), arreglado el estilo y tercerizado lo del login en un componente aparte

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -2,34 +2,80 @@ import React, { Component, Fragment } from "react";
 import estilos from "./Login.module.css";
 
 import Logo from "../../components/Logo/Logo";
+import IniciarSesion from "./IniciarSesion/IniciarSesion";
 import RecuperarContrasena from "./RecuperarContrasena/RecuperarContrasena";
 
 class Login extends Component {
+    state = {
+        mostrarIniciarSesion: true,
+        contrasenaIncorrecta: false,
+        cuentaBloqueada: false,
+    }
+
     // metodo para hacer el login
-    loginHandler() {
+    loginHandler = () => {
         // mandar al server usuario y contraseña
-        // si es incorrecto hacxer visible el div de clase estilos.Error
+        // si es incorrecto hacer visible el div de clase estilos.Error
         // si es correcto hacer un if
         //     si el usuario es un supervisor cargar el componente LayoutSupervisor
         //     si el usuario es un administrador cargar el componente LayoutAdministrador
     }
 
+    // metodo para mostrar el cuadro de recuperar contraseña
+    olvidarContrasenaHandler = () => {
+        this.setState({mostrarIniciarSesion: false});
+    }
+
+    // metodo para recuperar la contraseña
+    recuperarContrasenaHandler = () => {
+        // aca va el codigo para mandar el mail
+    }
+
+    // metood para volver a mostrar el cuadro de iniciar sesión
+    cancelarOlvidoHandler = () => {
+        this.setState({mostrarIniciarSesion: true});
+    }
+
     render() {
+        let componenteParaMostrar = (
+            <IniciarSesion
+                iniciar={this.loginHandler}
+                olvidar={this.olvidarContrasenaHandler}
+            />
+        );
+        if (!this.state.mostrarIniciarSesion) {
+            componenteParaMostrar = (
+                <RecuperarContrasena
+                    recuperar={this.recuperarContrasenaHandler}
+                    cancelar={this.cancelarOlvidoHandler}
+                />
+            );
+        }
+
+        let errorParaMostrar = null;
+        if (this.state.contrasenaIncorrecta) {
+            errorParaMostrar = (
+                <div className={estilos.Error}>
+                    El correo electrónico o la contraseña son incorrectos
+                </div>
+            );
+        }
+        if (this.state.cuentaBloqueada) {
+            errorParaMostrar = (
+                <div className={estilos.Error}>
+                    Ha ingresado una contraseña incorrecta 3 veces seguidas
+                </div>
+            );
+        }
+
         return (
             <Fragment>
                 <div className={estilos.Background}></div>
                 <div className={estilos.Controles}>
                     <Logo width={90} height={90} />
                     <h1>MultApp</h1>
-                    <div className={estilos.Error}>
-                        El correo electrónico o la contraseña son incorrectos
-                    </div>
-                    <label for="email">Correo Electrónico</label>
-                    <input type="text" id="email" />
-                    <label for="contrasena">Contraseña</label>
-                    <input type="password" id="contrasena" />
-                    <button onClick={this.loginHandler}>Iniciar sesión</button>
-                    <a href="">¿Olvidó su contraseña?</a>
+                    {errorParaMostrar}
+                    {componenteParaMostrar}
                 </div>
                 <div className={estilos.Footer}>
                     © 2020 El grupo de Acosta, Cardozo, García, Ibáñez y Mansilla. Todos los derechos reservados.
