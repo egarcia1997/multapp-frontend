ba1d6afae801d6eee295117c6494294c2efcd82f
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-05 23:00:43 -0300

    cosas basicas del perfil (no tiene nada todavia)

---
+++ src/components/Perfil/Perfil.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import axios from "axios";
+import estilos from "./Perfil.module.css";
+
+class Perfil extends Component {
+    render() {
+        return (
+            <div>el coso del perfil del empleado</div>
+        );
+    };
+}
+
+export default Perfil;
