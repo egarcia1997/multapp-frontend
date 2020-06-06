3d110da5f2432a89777267f72200a98dbb08932b
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-05 22:48:02 -0300

    agregadas rutas para ver las multas y el perfil del usuario

--- src/containers/LayoutSupervisor/LayoutSupervisor.js
+++ src/containers/LayoutSupervisor/LayoutSupervisor.js
@@ -1,13 +1,18 @@
 import React, { Component, Fragment } from "react";
+import {Switch, Route, Redirect} from "react-router-dom";
 import BarraSuperior from "../../components/BarraSuperior/BarraSuperior";
 import MultasResumidas from "../../components/MultasResumidas/MultasResumidas";
+import Perfil from "../../components/Perfil/Perfil";
 
 class LayoutSupervisor extends Component {
     render() {
         return (
             <Fragment>
                 <BarraSuperior nombre={this.props.usuario.nombre} imagen={this.props.usuario.imagen} />
-                <MultasResumidas nombreUsuario={this.props.usuario.nombre} />
+                <Switch>
+                    <Route path="/multas" render={() => <MultasResumidas nombreUsuario={this.props.usuario.nombre} />} />
+                    <Route path="/perfil" component={Perfil} />
+                </Switch>
             </Fragment>
         );
     }
