dae0da329a91878e02dccaca82d56a0a10454c72
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-08 22:36:49 -0300

    usando el componente DropdownMenu en la barra superior

--- src/components/BarraSuperior/BarraSuperior.js
+++ src/components/BarraSuperior/BarraSuperior.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { Fragment, Component } from "react";
 import estilos from "./BarraSuperior.module.css";
 import * as applogo from "../../assets/multapp-logo.png";
+import DropdownMenu from "./DropdownMenu/DropdownMenu";
 
-const BarraSuperior = (props) => {
-    return (
-        <div className={estilos.BarraSuperior}>
-            {/* los elementos en el div de clase derecha se van a mostrar al reves de como estan aca */}
-            {/* porque en el css tiene el estilo flex-direction: row-reverse */}
-            <div className={estilos.Izquierda}>
-                <img src={applogo} alt="Logo de MultApp" width="48" height="48" />
-                <span>MultApp</span>
-            </div>
-            <div className={estilos.Derecha}>
-                <img src={props.imagen} alt="Su foto de perfil" width="48" height="48" />
-                <span>{props.nombre}</span>
-            </div>
-        </div>
-    );
+class BarraSuperior extends Component {
+    state = {
+        mostrarDropdownMenu: false,
+    }
+
+    mostrarDropdownHandler = () => {
+        const nuevoValor = !this.state.mostrarDropdownMenu;
+        this.setState({mostrarDropdownMenu: nuevoValor});
+    }
+
+    render() {
+        let dropdownMenu = this.state.mostrarDropdownMenu ? <DropdownMenu /> : null;
+        return (
+            <Fragment>
+                <div className={estilos.BarraSuperior}>
+                    {/* los elementos en el div de clase derecha se van a mostrar al reves de como estan aca */}
+                    {/* porque en el css tiene el estilo flex-direction: row-reverse */}
+                    <div className={estilos.Izquierda}>
+                        <img src={applogo} alt="Logo de MultApp" width="48" height="48" />
+                        <span>MultApp</span>
+                    </div>
+                    <div className={estilos.Derecha} onClick={this.mostrarDropdownHandler}>
+                        <img src={this.props.imagen} alt="Su foto de perfil" width="48" height="48" />
+                        <span>{this.props.nombre}</span>
+                    </div>
+                </div>
+                {dropdownMenu}
+            </Fragment>
+        );
+    }
 }
 
 export default BarraSuperior;
