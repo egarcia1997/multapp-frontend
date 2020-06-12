8f3f97aad7b9a34846a3f7dedb6887f88c986c87
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-12 13:04:31 -0300

    reemplazando BarraSuperior por AppBar de material-ui

--- src/containers/LayoutSupervisor/LayoutSupervisor.js
+++ src/containers/LayoutSupervisor/LayoutSupervisor.js
@@ -4,12 +4,26 @@ import BarraSuperior from "../../components/BarraSuperior/BarraSuperior";
 import MultasResumidas from "../../components/MultasResumidas/MultasResumidas";
 import Perfil from "../../components/Perfil/Perfil";
 import estilos from "../Layout.module.css";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@material-ui/core";
+import { Menu, AccountCircle } from "@material-ui/icons";
 
 class LayoutSupervisor extends Component {
     render() {
         return (
             <div className={estilos.Layout}>
-                <BarraSuperior nombre={this.props.usuario.nombre} imagen={this.props.usuario.imagen} />
+                <AppBar position="sticky">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Menu />
+                        </IconButton>
+                        <Typography variant="h6" style={{flexGrow: "1"}}>
+                            MultApp
+                        </Typography>
+                        <IconButton edge="end" color="inherit" aria-label="menu">
+                            <AccountCircle />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
                 <div className={estilos.Contenido}>
                     <Switch>
                         <Route path="/multas" render={() => <MultasResumidas nombreUsuario={this.props.usuario.nombre} />} />
