d799096e64e25202d8070d3246179b5e78ca3a94
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-13 22:52:34 -0300

    tercerizando la AppBar en el componente BarraSuperior

--- src/containers/LayoutSupervisor/LayoutSupervisor.js
+++ src/containers/LayoutSupervisor/LayoutSupervisor.js
@@ -3,64 +3,14 @@ import {Switch, Route, Redirect, withRouter} from "react-router-dom";
 import MultasResumidas from "../../components/MultasResumidas/MultasResumidas";
 import Perfil from "../../components/Perfil/Perfil";
 import estilos from "../Layout.module.css";
-import Logo from "../../components/Logo/Logo";
-// aca hay conflicto, en /core e /icons hay un componente llamado Menu
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from "@material-ui/core";
-import { Menu as MenuIcon, AccountCircle } from "@material-ui/icons";
+import BarraSuperior from "../../components/BarraSuperior/BarraSuperior";
 
 class LayoutSupervisor extends Component {
-    state = {
-        mostrarMenu: false,
-    }
-
-    // metodo para abrir el menu
-    abrirMenuHandler = () => {
-        this.setState({mostrarMenu: true});
-    }
-
-    // metodo para cerrar el menu
-    cerrarMenuHandler = () => {
-        this.setState({mostrarMenu: false});
-    }
-
-    // metodo que te manda a la pagina para administrar las multas
-    multasHandler = () => {
-        this.cerrarMenuHandler();
-        this.props.history.push("/multas");
-    }
-
-    // metodo que te manda a tu perfil
-    perfilHandler = () => {
-        this.cerrarMenuHandler();
-        this.props.history.push("/perfil");
-    }
-
-    // metodo para cerrar sesion
-    cerrarSesionHandler = () => {
-        this.cerrarMenuHandler();
-    }
 
     render() {
         return (
             <div className={estilos.Layout}>
-                <AppBar position="sticky">
-                    <Toolbar variant="dense">
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <MenuIcon />{/* aca se tendria que mostrar el logo de multapp */}
-                        </IconButton>
-                        <Typography variant="h6" style={{flexGrow: "1"}}>
-                            MultApp
-                        </Typography>
-                        <IconButton edge="end" color="inherit" aria-label="menu" aria-haspopup="true" onClick={this.abrirMenuHandler}>
-                            <AccountCircle />
-                        </IconButton>
-                        <Menu id="menu" open={this.state.mostrarMenu} onClose={this.cerrarMenuHandler}>
-                            <MenuItem onClick={this.multasHandler}>Administrar multas</MenuItem>
-                            <MenuItem onClick={this.perfilHandler}>Mi perfil</MenuItem>
-                            <MenuItem onClick={this.cerrarSesionHandler}>Cerrar sesión</MenuItem>
-                        </Menu>
-                    </Toolbar>
-                </AppBar>
+                <BarraSuperior />
                 <div className={estilos.Contenido}>
                     <Switch>
                         <Route path="/multas" render={() => <MultasResumidas nombreUsuario={this.props.usuario.nombre} />} />
