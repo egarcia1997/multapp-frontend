1bb7cb6c3aa9a3bd991558118778d674fe1bc64c
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-17 22:41:52 -0300

    eliminado un div innecesario

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import * as placeholder from "./assets/placeholder-vault-boy.png";
 import Login from "./containers/Login/Login";
 import LayoutSupervisor from './containers/LayoutSupervisor/LayoutSupervisor';
 
 class App extends Component{
-  state = {
-    usuario:
-    {
-      nombre: "Juan Pérez",
-      imagen: placeholder,
+    state = {
+        usuario: {
+            nombre: "Juan Pérez",
+            imagen: placeholder,
+        }
     }
-  }
 
-  render() {
-    return (
-      <Fragment>
-          <Login />
-          {/* <LayoutSupervisor usuario={this.state.usuario} /> */}
-      </Fragment>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                {/* <Login /> */}
+                <LayoutSupervisor usuario={this.state.usuario} />
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
