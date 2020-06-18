1d8212e92a4d749649f3fadda763bcd6bcb9ea14
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-17 22:51:04 -0300

    usando Box y Container

--- src/containers/LayoutSupervisor/LayoutSupervisor.js
+++ src/containers/LayoutSupervisor/LayoutSupervisor.js
@@ -4,22 +4,23 @@ import MultasResumidas from "../../components/MultasResumidas/MultasResumidas";
 import Perfil from "../../components/Perfil/Perfil";
 import estilos from "../Layout.module.css";
 import BarraSuperior from "../../components/BarraSuperior/BarraSuperior";
+import { Container, Box } from "@material-ui/core";
 
 class LayoutSupervisor extends Component {
 
     render() {
         return (
-            <div className={estilos.Layout}>
+            <Box className={estilos.Layout}>
                 <BarraSuperior />
-                <div className={estilos.Contenido}>
+                <Container className={estilos.Contenido}>
                     <Switch>
                         <Route path="/multas" render={() => <MultasResumidas nombreUsuario={this.props.usuario.nombre} />} />
                         <Route path="/perfil" component={Perfil} />
                         {/* esto es temporal, esta para probar nomas */}
                         <Redirect from="/" to="/multas" />
                     </Switch>
-                </div>
-            </div>
+                </Container>
+            </Box>
         );
     }
 }
