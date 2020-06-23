f8635c7151e07e0d8883a1cfcd7598c7307d964c
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-23 17:20:34 -0300

    creado el componente Usuarios

---
+++ src/components/Usuarios/Usuarios.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container } from "@material-ui/core";
+
+class Usuarios extends Component {
+    render() {
+        return (
+            <Container maxWidth="lg">
+
+            </Container>
+        );
+    }
+}
+
+export default Usuarios;
