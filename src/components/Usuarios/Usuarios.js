d326a887005f097a1e6f476646a8c7bc091f446f
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-25 18:05:36 -0300

    agregado un boton para agregar un nuevo usuario

--- src/components/Usuarios/Usuarios.js
+++ src/components/Usuarios/Usuarios.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
-import { Box, Container, Typography, Tabs, Tab, CircularProgress, TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell, List, ListItem, Divider, ListItemAvatar, Avatar, ListItemText } from "@material-ui/core";
+import { Box, Container, Typography, Tabs, Tab, CircularProgress, TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell, List, ListItem, Divider, ListItemAvatar, Avatar, ListItemText, Fab } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
+import AgregarUsuario from "./AgregarUsuario/AgregarUsuario";
 
 class Usuarios extends Component {
     state = {
@@ -8,6 +10,7 @@ class Usuarios extends Component {
         cargando: true,
         huboError: false,
         textoDeError: "",
+        agregarUsuario: false,
     }
 
     componentDidMount = () => {
@@ -51,39 +54,50 @@ class Usuarios extends Component {
         this.setState({pestanaActual: newValue});
     }
     
+    addUserHandler = () => {
+        const nuevoEstado = !this.state.agregarUsuario;
+        this.setState({agregarUsuario: nuevoEstado});
+    }
+
     render() {
         return (
-            <Container maxWidth="lg">
-                <Tabs value={this.state.pestanaActual} centered={true} onChange={this.tabChangeHandler} indicatorColor="primary" textColor="primary">
-                    <Tab label="Inspectores" />
-                    <Tab label="Supervisores" />
-                    <Tab label="Administradores" />
-                    <Tab label="Multados" />
-                </Tabs>
-                <this.TabPanel value={this.state.pestanaActual} index={0}>
-                    {this.state.cargando ?
-                        <CircularProgress />
-                    : "Coso de inspectores"}
-                </this.TabPanel>
-                <this.TabPanel value={this.state.pestanaActual} index={1}>
-                    <List>
-                        <ListItem button={true}>
-                            <ListItemAvatar>
-                                <Avatar />
-                            </ListItemAvatar>
-                            <ListItemText primary="Juan Pérez" secondary="64ghf8shj23" />
-                        </ListItem>
-                        <Divider />
-                    </List>
-                    Coso de supervisores
-                </this.TabPanel>
-                <this.TabPanel value={this.state.pestanaActual} index={2}>
-                    Coso de administradores
-                </this.TabPanel>
-                <this.TabPanel value={this.state.pestanaActual} index={3}>
-                    Coso de multados
-                </this.TabPanel>
-            </Container>
+            <Fragment>
+                <Container maxWidth="lg">
+                    <Tabs value={this.state.pestanaActual} centered={true} onChange={this.tabChangeHandler} indicatorColor="primary" textColor="primary">
+                        <Tab label="Inspectores" />
+                        <Tab label="Supervisores" />
+                        <Tab label="Administradores" />
+                        <Tab label="Multados" />
+                    </Tabs>
+                    <this.TabPanel value={this.state.pestanaActual} index={0}>
+                        {this.state.cargando ?
+                            <CircularProgress />
+                        : "Coso de inspectores"}
+                    </this.TabPanel>
+                    <this.TabPanel value={this.state.pestanaActual} index={1}>
+                        <List>
+                            <ListItem button={true}>
+                                <ListItemAvatar>
+                                    <Avatar />
+                                </ListItemAvatar>
+                                <ListItemText primary="Juan Pérez" secondary="64ghf8shj23" />
+                            </ListItem>
+                            <Divider />
+                        </List>
+                        Coso de supervisores
+                    </this.TabPanel>
+                    <this.TabPanel value={this.state.pestanaActual} index={2}>
+                        Coso de administradores
+                    </this.TabPanel>
+                    <this.TabPanel value={this.state.pestanaActual} index={3}>
+                        Coso de multados
+                    </this.TabPanel>
+                    <Fab color="primary" onClick={this.addUserHandler}>
+                        <Add />
+                    </Fab>
+                </Container>
+                <AgregarUsuario open={this.state.agregarUsuario} onClose={this.addUserHandler} />
+            </Fragment>
         );
     }
 }
