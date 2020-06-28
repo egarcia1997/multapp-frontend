57fa7213b73eaa1777b6ce0802b1cdf626cedefd
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-06-27 22:58:27 -0300

    two-way binding en todos los input e intento de funcion para cargar los usuarios

--- src/components/Usuarios/AgregarUsuario/AgregarUsuario.js
+++ src/components/Usuarios/AgregarUsuario/AgregarUsuario.js
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Typography, Radio, RadioGroup, FormLabel, FormControlLabel, FormControl, Grid, DialogContentText } from "@material-ui/core";
 
 class AgregarUsuario extends Component {
+    state = {
+        dni: "",
+        apellido: "",
+        nombre: "",
+        fechaNacimiento: new Date().toISOString().slice(0, 10),
+        sexo: "masculino",
+        calle: "",
+        numero: "",
+        piso: "",
+        departamento: "",
+        localidad: "",
+        provincia: "",
+        foto: null,
+    }
+
+    // carga lo que escribe el usuario en el state
+    inputHandler = (event) => {
+        this.setState({[event.target.id]: event.target.value});
+    }
+    
+    // carga el radio que selecciona el usuario en el state
+    radioHandler = (event) => {
+        this.setState({sexo: event.target.value});
+    }
+
     render() {
         return (
             <Dialog open={this.props.open} onClose={this.props.onClose} maxWidth="xl" fullWidth={true}>
@@ -14,12 +39,40 @@ class AgregarUsuario extends Component {
                         <Grid item={true} xs={4}>
                             <FormControl fullWidth={true}>
                                 <FormLabel>Datos personales</FormLabel>
-                                <TextField type="number" label="DNI" required={true} />
-                                <TextField type="text" label="Apellido" required={true} />
-                                <TextField type="text" label="Nombre" required={true} />
-                                <TextField type="date" label="Fecha de nacimiento" required={true} />
+                                <TextField
+                                    id="dni"
+                                    type="number"
+                                    label="DNI"
+                                    required={true}
+                                    value={this.state.dni}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="apellido"
+                                    type="text"
+                                    label="Apellido"
+                                    required={true}
+                                    value={this.state.apellido}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="nombre"
+                                    type="text"
+                                    label="Nombre"
+                                    required={true}
+                                    value={this.state.nombre}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="fechaNacimiento"
+                                    type="date"
+                                    label="Fecha de nacimiento"
+                                    required={true}
+                                    value={this.state.fechaNacimiento}
+                                    onChange={this.inputHandler}
+                                />
                                 <FormLabel>Sexo</FormLabel>
-                                <RadioGroup>
+                                <RadioGroup value={this.state.sexo} onChange={this.radioHandler}>
                                     <FormControlLabel value="masculino" label="Masculino" control={<Radio />} />
                                     <FormControlLabel value="femenino" label="Femenino" control={<Radio />} />
                                 </RadioGroup>
@@ -28,12 +81,51 @@ class AgregarUsuario extends Component {
                         <Grid item={true} xs={4}>
                             <FormControl fullWidth={true}>
                                 <FormLabel>Dirección</FormLabel>
-                                <TextField type="text" label="Calle" required={true} />
-                                <TextField type="number" label="Número" />
-                                <TextField type="text" label="Piso" />
-                                <TextField type="text" label="Departamento" />
-                                <TextField type="text" label="Localidad" required={true} />
-                                <TextField type="text" label="Provincia" required={true} />
+                                <TextField
+                                    id="calle"
+                                    type="text"
+                                    label="Calle"
+                                    required={true}
+                                    value={this.state.calle}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="numero"
+                                    type="number"
+                                    label="Número"
+                                    value={this.state.numero}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="piso"
+                                    type="text"
+                                    label="Piso"
+                                    value={this.state.piso}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="departamento"
+                                    type="text"
+                                    label="Departamento"
+                                    value={this.state.departamento}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="localidad"
+                                    type="text"
+                                    label="Localidad"
+                                    required={true}
+                                    value={this.state.localidad}
+                                    onChange={this.inputHandler}
+                                />
+                                <TextField
+                                    id="provincia"
+                                    type="text"
+                                    label="Provincia"
+                                    required={true}
+                                    value={this.state.provincia}
+                                    onChange={this.inputHandler}
+                                />
                             </FormControl>
                         </Grid>
                         <Grid item={true} xs={4}>
@@ -45,7 +137,7 @@ class AgregarUsuario extends Component {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={this.props.onClose}>Cancelar</Button>
-                    <Button color="primary">Agregar</Button>
+                    <Button onClick={this.agregarUsuario} color="primary">Agregar</Button>
                 </DialogActions>
             </Dialog>
         )
