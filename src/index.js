ad19e13c4226ac5d33cf90127834e459cb1d2669
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-01 22:43:59 -0300

    conectando esta abominacion con el choclo de redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from "axios";
+import Axios from "axios";
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import multasReducer from "./store/reducers/multas";
+import usuariosReducer from "./store/reducers/usuarios";
 
-axios.defaults.baseURL = "https://multa-app.herokuapp.com"; // esto es para usar la misma url en todas las peticiones http sin tener que andar escribiendo
+Axios.defaults.baseURL = "https://multa-app.herokuapp.com"; // esto es para usar la misma url en todas las peticiones http sin tener que andar escribiendo
+
+const reducers = combineReducers(multasReducer, usuariosReducer);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
