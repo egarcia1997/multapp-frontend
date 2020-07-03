ab1887f9a2c36dbcedc3297abd93a0cef344ac11
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-02 22:47:20 -0300

    cargando los return de cargar multa con exito y con error

--- src/store/reducers/multa.js
+++ src/store/reducers/multa.js
@@ -1,13 +1,26 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const initialState = {};
+const initialState = {
+    multa: null,
+    cargando: true,
+    error: false,
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.CARGAR_MULTA_CON_EXITO:
-            return {};
+            return {
+                ...state,
+                multa: action.multa,
+                cargando: false,
+                error: false,
+            };
         case actionTypes.CARGAR_MULTA_CON_ERROR:
-            return {};
+            return {
+                ...state,
+                cargando: false,
+                error: action.error,
+            };
         case actionTypes.CAMBIAR_ESTADO_CON_EXITO:
             return {};
         case actionTypes.CAMBIAR_ESTADO_CON_ERROR:
