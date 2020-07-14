6098a9db1fc537c0f60bf2e069a809e4e26b5408
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-14 19:34:38 -0300

    tercerizando la traduccion de los errores de firebase en su propio archivo

---
+++ src/share/traducirError.js
@@ -0,0 +1,15 @@
+// funcion a la que se le mete un error que da firebase y devuelve algo entendible
+const traducirError = (error) => {
+    switch (error) {
+        case "INVALID_PASSWORD":
+            return "La contraseña es incorrecta";
+        case "EMAIL_NOT_FOUND":
+            return "El correo electrónico no está cargado en nuestros sistemas";
+        case "INVALID_EMAIL":
+            return "El correo electrónico ingresado no es válido";
+        case "USER_DISABLED":
+            return "Su cuenta fue bloqueada. Contacte con un administrador";
+        default:
+            return "Ocurrió un error. Intente nuevamente";
+    }
+}
