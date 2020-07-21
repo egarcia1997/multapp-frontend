635efe741574123fd35efc8f966e891d87bddb0e
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-20 22:48:40 -0300

    creadas acciones y reducer de usuario

---
+++ src/store/actions/usuario.js
@@ -0,0 +1,38 @@
+import * as actionTypes from "./actionTypes";
+import Axios from "axios";
+
+const cargarUsuarioConExito = (usuario) => {
+    return {
+        type: actionTypes.CARGAR_MULTA_CON_EXITO,
+        usuario: usuario,
+    }
+}
+
+const cargarUsuarioConError = (error) => {
+    return {
+        type: actionTypes.CARGAR_MULTA_CON_ERROR,
+        error: error,
+    }
+}
+
+export const cargarUsuario = (id) => {
+    const params = {
+        headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        params: {
+            id: id,
+        },
+    };
+    return dispatch => {
+        Axios.get("/getUsuario", params)
+            .then(response => {
+                console.log(response);
+                dispatch(cargarUsuarioConExito(response.data));
+            }).catch(error => {
+                console.log(error);
+                dispatch(cargarUsuarioConError(error));
+            });
+    }
+}
---
+++ src/store/reducers/usuario.js
@@ -0,0 +1,34 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+    usuario: null,
+    cargando: true,
+    errorAlCargar: false,
+    textoDeErrorAlCargar: false,
+    estadoCambiado: false,
+    errorAlModificarDatos: false,
+    textoDeErrorAlModificarDatos: false,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.CARGAR_USUARIO_CON_EXITO:
+            return {
+                ...state,
+                usuario: action.usuario,
+                cargando: false,
+                errorAlCargar: false,
+            };
+        case actionTypes.CARGAR_USUARIO_CON_ERROR:
+            return {
+                ...state,
+                cargando: false,
+                errorAlCargar: true,
+                textoDeErrorAlCargar: action.error,
+            };
+        default:
+            return state;
+        }
+}
+
+export default reducer;
