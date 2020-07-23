6eb3d445ebad00ff693211fa89091ac9ada5c864
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-07-22 22:27:44 -0300

    renombrando acciones de crear a editar

--- src/store/actions/editarUsuario.js
+++ src/store/actions/editarUsuario.js
@@ -3,19 +3,19 @@ import Axios from "axios";
 
 const editarUsuarioStart = () => {
     return {
-        type: actionTypes.CREAR_USUARIO_START,
+        type: actionTypes.EDITAR_USUARIO_START,
     }
 }
 
 const editarUsuarioConExito = () => {
     return {
-        type: actionTypes.CREAR_USUARIO_CON_EXITO,
+        type: actionTypes.EDITAR_USUARIO_CON_EXITO,
     }
 }
 
 const editarUsuarioConError = (error) => {
     return {
-        type: actionTypes.CREAR_USUARIO_CON_ERROR,
+        type: actionTypes.EDITAR_USUARIO_CON_ERROR,
         error: error,
     }
 }
@@ -32,7 +32,7 @@ export const editarUsuario = (usuario, foto, editar) => {
             "Access-Control-Allow-Origin": "*",
         };
         // si editar es true, edita el usuario, si es false crea uno nuevo
-        let url = editar ? "/editUsuario" : "/addUsuario"
+        let url = editar ? "/editUsuario" : "/addUsuario";
         Axios.post(url, data, headers)
             .then(response => {
                 dispatch(editarUsuarioConExito());
