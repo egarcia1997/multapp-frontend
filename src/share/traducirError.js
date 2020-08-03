a6b9db2c758f3cb6a1fb0b91c8e12158df60f006
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-02 23:04:05 -0300

    traduciendo errores de authentication

--- src/share/traducirError.js
+++ src/share/traducirError.js
@@ -1,6 +1,7 @@
 // funcion a la que se le mete un error que da firebase y devuelve algo entendible
 export const traducirError = (error) => {
     switch (error) {
+        // errores de login
         case "INVALID_PASSWORD":
             return "La contraseña es incorrecta";
         case "EMAIL_NOT_FOUND":
@@ -9,6 +10,11 @@ export const traducirError = (error) => {
             return "El correo electrónico ingresado no es válido";
         case "USER_DISABLED":
             return "Su cuenta fue bloqueada. Contacte con un administrador";
+        // errores de authentication
+        case "auth/email-already-exists":
+            return "Ya existe un usuario con el correo electrónico ingresado";
+        case "auth/phone-number-already-exists":
+            return "Ya existe un usuario con el número de teléfono ingresado";
         default:
             return "Ocurrió un error. Intente nuevamente";
     }
