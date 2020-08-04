4b388c9598053d151018dc22eb61c8590e197cd8
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-08-03 22:00:11 -0300

    convirtiendo CambiarContrasena a componente funcional

--- src/components/CambiarContrasena/CambiarContrasena.js
+++ src/components/CambiarContrasena/CambiarContrasena.js
@@ -1,100 +1,99 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button, CircularProgress } from "@material-ui/core";
 import { cambiarContrasena, abrirDialogCambiarContrasena, cerrarDialogCambiarContrasena } from "../../store/actions/cambiarContrasena";
+import useStyles from "../../share/useStyles";
 
-class CambiarContrasena extends Component {
-    state = {
-        contrasenaActual: "",
-        nuevaContrasena: "",
-        repitaContrasena: "",
-        errorNuevaContrasena: "",
-        errorRepitaContrasena: "",
-        aceptable: false,
-    }
+const CambiarContrasena = props => {
+    const [contrasenaActual, setContrasenaActual] = useState("");
+    const [nuevaContrasena, setNuevaContrasena] = useState("");
+    const [repitaContrasena, setRepitaContrasena] = useState("");
+    const [errorNuevaContrasena, setErrorNuevaContrasena] = useState("");
+    const [errorRepitaContrasena, setErrorRepitaContrasena] = useState("");
+    const [aceptable, setAceptable] = useState(false);
 
-    // carga lo que escribe el usuario en el state
-    inputHandler = (event) => {
-        this.setState({[event.target.id]: event.target.value}, () => {
-            if (this.state.contrasenaActual !== "" || this.state.nuevaContrasena !== "" || this.state.repitaContrasena !== "") {
-                this.setState({aceptable: true});
-            }
-            if (this.state.contrasenaActual === this.state.nuevaContrasena) {
-                this.setState({errorNuevaContrasena: "La nueva contraseña es igual a la actual", aceptable: false});
-            }
-            else {
-                this.setState({errorNuevaContrasena: ""});
-            }
-            if (this.state.nuevaContrasena.length < 6) {
-                this.setState({errorNuevaContrasena: "La contraseña debe tener 6 o más caracteres", aceptable: false});
-            }
-            else {
-                this.setState({errorNuevaContrasena: ""});
-            }
-            if (this.state.nuevaContrasena !== this.state.repitaContrasena) {
-                this.setState({errorRepitaContrasena: "Las contraseñas no coinciden", aceptable: false});
-            }
-            else {
-                this.setState({errorRepitaContrasena: ""});
-            }
-        });
-    }
+    const estilos = useStyles();
 
-    render() {
-        return (
-            <Dialog open={this.props.open} onClose={this.props.onClose}>
-                <DialogTitle>Cambiar contraseña</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        La contraseña debe tener como mínimo 6 caracteres.<br />
-                        Recomendamos que utilice mayúsculas, minúsculas, números y símbolos.
-                    </DialogContentText>
-                    <TextField
-                        autoFocus={true}
-                        type="password"
-                        id="contrasenaActual"
-                        label="Contraseña actual"
-                        fullWidth={true}
-                        value={this.state.contrasenaActual}
-                        onChange={this.inputHandler}
-                    />
-                    <TextField
-                        type="password"
-                        id="nuevaContrasena"
-                        label="Nueva contraseña"
-                        fullWidth={true}
-                        helperText={this.state.errorNuevaContrasena}
-                        error={this.state.errorNuevaContrasena !== ""}
-                        value={this.state.nuevaContrasena}
-                        onChange={this.inputHandler}
-                    />
-                    <TextField
-                        type="password"
-                        id="repitaContrasena"
-                        label="Repita la nueva contraseña"
-                        fullWidth={true}
-                        helperText={this.state.errorRepitaContrasena}
-                        error={this.state.errorRepitaContrasena !== ""}
-                        value={this.state.repitaContrasena}
-                        onChange={this.inputHandler}
-                    />
-                    <DialogActions>
-                        <Button onClick={this.props.onClose} color="default">
-                            Cancelar
-                        </Button>
-                        <Button
-                            onClick={() => this.props.cambiarContrasena(this.state.contrasenaActual, this.state.nuevaContrasena)}
-                            color="primary"
-                            disabled={!this.state.aceptable}
-                        >
-                            Aceptar
-                            {this.props.cargando && <CircularProgress size={24} />}
-                        </Button>
-                    </DialogActions>
-                </DialogContent>
-            </Dialog>
-        )
-    }
+    useEffect(() => {
+        if (contrasenaActual !== "" && nuevaContrasena !== "" && repitaContrasena !== "") {
+            setAceptable(true);
+        }
+        if (contrasenaActual === nuevaContrasena) {
+            setErrorNuevaContrasena("La nueva contraseña es igual a la actual");
+            setAceptable(false);
+        }
+        else {
+            setErrorNuevaContrasena("");
+        }
+        if (nuevaContrasena.length < 6) {
+            setErrorNuevaContrasena("La contraseña debe tener 6 o más caracteres");
+            setAceptable(false);
+        }
+        else {
+            setErrorNuevaContrasena("");
+        }
+        if (nuevaContrasena !== repitaContrasena) {
+            setErrorRepitaContrasena("Las contraseñas no coinciden");
+            setAceptable(false);
+        }
+        else {
+            setErrorRepitaContrasena("");
+        }
+    }, [contrasenaActual, nuevaContrasena, repitaContrasena]);
+    
+    return (
+        <Dialog open={props.open} onClose={props.onClose}>
+            <DialogTitle>Cambiar contraseña</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    La contraseña debe tener como mínimo 6 caracteres.<br />
+                    Recomendamos que utilice mayúsculas, minúsculas, números y símbolos.
+                </DialogContentText>
+                <TextField
+                    autoFocus={true}
+                    type="password"
+                    id="contrasenaActual"
+                    label="Contraseña actual"
+                    fullWidth={true}
+                    value={contrasenaActual}
+                    onChange={event => setContrasenaActual(event.target.value)}
+                />
+                <TextField
+                    type="password"
+                    id="nuevaContrasena"
+                    label="Nueva contraseña"
+                    fullWidth={true}
+                    helperText={errorNuevaContrasena}
+                    error={errorNuevaContrasena !== ""}
+                    value={nuevaContrasena}
+                    onChange={event => setNuevaContrasena(event.target.value)}
+                />
+                <TextField
+                    type="password"
+                    id="repitaContrasena"
+                    label="Repita la nueva contraseña"
+                    fullWidth={true}
+                    helperText={errorRepitaContrasena}
+                    error={errorRepitaContrasena !== ""}
+                    value={repitaContrasena}
+                    onChange={event => setRepitaContrasena(event.target.value)}
+                />
+                <DialogActions>
+                    <Button onClick={props.onClose} color="default">
+                        Cancelar
+                    </Button>
+                    <Button
+                        onClick={() => props.cambiarContrasena(contrasenaActual, nuevaContrasena)}
+                        color="primary"
+                        disabled={!aceptable || props.cargando}
+                    >
+                        Aceptar
+                        {props.cargando && <CircularProgress size={24} className={estilos.buttonProgress} />}
+                    </Button>
+                </DialogActions>
+            </DialogContent>
+        </Dialog>
+    );
 }
 
 const mapStateToProps = state => {
